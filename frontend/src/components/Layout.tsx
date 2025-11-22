abe9bead5729c0777651b5f8172560a2ac6c681a
luk1441 <lukmy3@web.de>
2025-11-22 17:41:03 +0100

    Update UI - still not fixed

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,28 +1,23 @@
 import React from 'react'
- 
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 const Layout: React.FC = () => {
   return (
-    <>
+    <div className="layout-container">
       <header className="app-header">
         <Navbar />
       </header>
 
       <main className="app-body">
-        <div className="content-box">
-          <div className="content-inner">
-            <Outlet />
-          </div>
-        </div>
+        <Outlet />
       </main>
 
       <footer className="app-footer">
         <Footer />
       </footer>
-    </>
+    </div>
   )
 }
 
